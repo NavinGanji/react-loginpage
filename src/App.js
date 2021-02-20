0333972735bfb8ea81fe13f66495ca7236efdfa5
Navin <navinganji09@gmail.com>
2021-02-20 19:19:01 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import './main.css';
-import Compo3 from "./Components/Compo3"
+import Container from "./Components/Container"
 function App() {
   return (
     <div className="d-flex align-items-center min-vh-100 py-3 py-md-0">
-        <Compo3/>
+        <Container/>
     </div>
   );
 }
